a230b3ec52bd7d8a8e8e4bb5685e319bd69187fc
Yuvan Naidoo <yuvannaidoo@gmail.com>
2025-03-10 13:13:09 +0200

    Test

--- scripts/supaBase_client.js
+++ scripts/supaBase_client.js
@@ -1,5 +1,5 @@
 // Initialize Supabase client
 const { createClient } = window.supabase;
-const supabaseUrl = 'https://mmvlwutnuouynukotdey.supabase.co';
+const supabaseUrl = 'https://db.thediveclub.org';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tdmx3dXRudW91eW51a290ZGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2OTgyMzEsImV4cCI6MjA1NTI3NDIzMX0.qyEDq8w67G2BMfyHO7Iyvd3nFUSd0sulJhGl0eGkbfA';
 const supabase = createClient(supabaseUrl, supabaseKey);
