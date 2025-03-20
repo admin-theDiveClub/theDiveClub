884ff12922af36bc45cb8d776bbafb119f6df1db
UV_StrixLaptop <yuvannaidoo@gmail.com>
2025-03-20 02:05:46 +0200

    Test_FixSignOut

--- scripts/signOut.js
+++ scripts/signOut.js
@@ -8,6 +8,7 @@ async function SignOut()
     const { error } = await supabase.auth.signOut();
     if (error) 
     {
+        alert("Error signing out: " + error.message);
         console.error('Error signing out:', error.message);
     } else 
     {
