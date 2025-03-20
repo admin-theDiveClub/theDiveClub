1cfefa6808ae7e2b8064e4933e1a589ec1141cfa
UV_StrixLaptop <yuvannaidoo@gmail.com>
2025-03-20 02:44:55 +0200

    Experiment - SignOut (Refresh Session Before)

--- scripts/signOut.js
+++ scripts/signOut.js
@@ -5,16 +5,11 @@ document.getElementById('btn_signOut').addEventListener('click', () =>
 
 async function SignOut() 
 {
+    await supabase.auth.refreshSession();
     const { error } = await supabase.auth.signOut();
     if (error) 
     {
-        alert("Error signing out: " + error.message);
         console.error('Error signing out:', error.message);
-    } else 
-    {
-        localStorage.clear();
-        sessionStorage.clear();
-        window.location.reload();
     }
     localStorage.clear();
     sessionStorage.clear();
