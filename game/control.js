c68d4df3a28b367c254ceef218b03a2abd4e11b0
UV_StrixLaptop <yuvannaidoo@gmail.com>
2025-03-25 16:10:00 +0200

    Backup - Game

---
+++ game/control.js
@@ -0,0 +1,5 @@
+//Mark game instance as active
+//COntrols
+//When complete, a single function to update all submittions for this round with correct results
+//Compile leaderboard and store in game data
+    //then update main leaderboard in game
---
+++ game/game.js
@@ -0,0 +1,6 @@
+//Get Game Data
+    //If Game Round is active, then subscribe
+//Get User Subs
+//Get leaderboard from gamedata
+//Update UI
+//Submit Predictions
