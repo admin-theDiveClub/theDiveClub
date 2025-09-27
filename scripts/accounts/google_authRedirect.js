5752343e61428756dfdb5451536533bc3784a1b0
UV_StrixLaptop <yuvannaidoo@gmail.com>
2025-09-27 10:18:34 +0200

    Update

--- scripts/accounts/google_authRedirect.js
+++ scripts/accounts/google_authRedirect.js
@@ -11,9 +11,48 @@ async function Start ()
         {
             localStorage.setItem("session", JSON.stringify(response.data.session));
             sessionStorage.setItem("session", JSON.stringify(response.data.session));
+
+            await GetUserProfile(response.data.session.user.email);
+
             window.location.href = "../accounts/profile.html";
         }
         //console.log("Session: ", response);
     }
 }
 
+async function GetUserProfile (username)
+{
+    var s_userProfile = localStorage.getItem('userProfile') || sessionStorage.getItem('userProfile');
+    if (s_userProfile)
+    {
+        var userProfile = JSON.parse(s_userProfile);
+        if (userProfile && userProfile.id && userProfile.username && userProfile.username == username)
+        {
+            if (!userProfile.pp)
+            {
+                userProfile = await UpdatePlayerProfilePicture(userProfile);
+            }
+            return userProfile;
+        }
+    }
+     else
+    {
+        const response = await supabase.from('tbl_players').select('*').eq('username', username).single();
+        userProfile = response.data;
+
+        if (userProfile && userProfile.id)
+        {
+            userProfile = await UpdatePlayerProfilePicture(userProfile);
+
+            localStorage.setItem("userProfile", JSON.stringify(userProfile));
+            sessionStorage.setItem("userProfile", JSON.stringify(userProfile));
+
+            return userProfile;
+        } else
+        {
+            localStorage.removeItem("userProfile");
+            sessionStorage.removeItem("userProfile");
+            return null;
+        }
+    }
+}
